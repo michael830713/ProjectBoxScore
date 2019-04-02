3513752b6dace8557fcffe746b55d2384746661d
mike <michael830713@gmail.com>
2019-04-02 20:56:12 +0800

    add the MVP for login page

---
+++ app/src/main/java/com/mike/projectboxscore/loginUI/LoginUIFragment.java
@@ -0,0 +1,42 @@
+package com.mike.projectboxscore.loginUI;
+
+import android.support.v4.app.Fragment;
+import android.view.SurfaceHolder;
+
+public class LoginUIFragment extends Fragment implements LoginUIViewContract.View {
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void showFullscreenModeUi(int videoWidth, int videoHeight) {
+
+    }
+
+    @Override
+    public void showNormalModeUi(int videoWidth, int videoHeight) {
+
+    }
+
+    @Override
+    public void addSurfaceHolderCallback(SurfaceHolder.Callback callback) {
+
+    }
+
+    @Override
+    public void requestLandscapeUi() {
+
+    }
+
+    @Override
+    public void requestPortraitUi() {
+
+    }
+
+    @Override
+    public void setPresenter(LoginUIViewContract.Presenter surfaceViewPresenter) {
+
+    }
+}
---
+++ app/src/main/java/com/mike/projectboxscore/loginUI/LoginUIPresenter.java
@@ -0,0 +1,48 @@
+package com.mike.projectboxscore.loginUI;
+
+public class LoginUIPresenter implements LoginUIViewContract.Presenter {
+    @Override
+    public void result(int requestCode, int resultCode) {
+
+    }
+
+    @Override
+    public void doNormalMode(int screenWidth, int screenHeight) {
+
+    }
+
+    @Override
+    public void doFullscreenMode(int screenWidth, int screenHeight) {
+
+    }
+
+    @Override
+    public void setScreenInfo(int screenWidth, int screenHeight) {
+
+    }
+
+    @Override
+    public void setVideoInfo(int videoWidth, int videoHeight) {
+
+    }
+
+    @Override
+    public void calculateAndUpdateSurface() {
+
+    }
+
+    @Override
+    public void requestLandscape() {
+
+    }
+
+    @Override
+    public void requestPortrait() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+}
